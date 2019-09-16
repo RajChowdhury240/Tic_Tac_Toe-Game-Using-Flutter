19fb681c4909a3430754d8c66247bb0a09f64357
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2019-09-17 03:32:07 +0530

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,7 @@ class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => new _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
   List<GameButton> buttonsList;
   var player1;
